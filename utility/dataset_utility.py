39782888ffcf77b944d4380ccde45c6b38fa18bb
Ben Firner <ben.firner@gmail.com>
2023-08-09 16:24:53 -0400

    Allow for user-provided number of one-hot vector outputs.

--- utility/dataset_utility.py
+++ utility/dataset_utility.py
@@ -6,21 +6,22 @@ Utility functions for dataloading with webdatasets.
 import torch
 import webdataset as wds
 
-def getLabelSize(data_path, decode_strs, convert_idx_to_classes, label_index):
+def getLabelSize(data_path, decode_strs, convert_idx_to_classes, label_index, number_of_classes=3):
     """
     Arguments:
         data_path   (str or list[str]): Path to webdataset tar file(s).
         decode_strs (str): Decode string for dataset loading.
         convert_idx_to_classes (bool): True to convert single index values to one hot labels.
         label_index (int): The index of the label to check
+        number_of_classes (int): Number of one-hot classes, used if convert_idx_to_classes is True.
     Returns:
         label_size  (int): The size of labels in the dataset.
     """
-    # TODO Currently only set up to convert index labels to 3 class outputs. Need to add another
-    # program argument since there is no way to know all possible class values without reading the
-    # entire dataset.
+    # When converting to one hot vectors, there is no way to know all possible class values without
+    # reading the entire dataset. Thus an argument is required to tell the function how many classes
+    # exist.
     if convert_idx_to_classes:
-        return 3
+        return number_of_classes
     # Check the size of the labels
     test_dataset = (
         wds.WebDataset(data_path)
