752d4d4b243580d128c117358e0a358cccd36877
Ben Firner <ben.firner@gmail.com>
2023-10-06 20:49:46 -0400

    More poking at the sparse net thing.

--- models/sparse_net.py
+++ models/sparse_net.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 class SparseEncodingNet(nn.Module):
     """A convolution, then a modulation, then a n-way hash, thresholding, and prediction+feedback modulation."""
 
-    def createConvolutions(self, width, depth, input_size, activation=nn.Step):
+    def createConvolutions(self, width, depth, input_size, kc_repititions, activation=nn.Step):
         """Create 3x3 convolution layers of the given width and depth."""
         layers = []
         padding = 1
@@ -34,15 +34,29 @@ class SparseEncodingNet(nn.Module):
                 # weights 0.5, three with 0.3, etc. Something like that.
                 # May be simpler to set the weight to a negative value so that all the weights must
                 # be used to get a positive output and trigger the step activation function
+                # For now, we'll just be using the orthogonal initialization without digging further
+                # into this.
+                torch.nn.init.orthogonal_(layers[-1].weight, gain=1)
+                # TODO The bias? Set them all to the same value and then gradually lower if a layer
+                # is overactive? Ideally, should each output be active half the time so that they
+                # are all super-sensitive and then tuning happens in the KC-like layer? Should the
+                # biases start extremely high and then gradually lower until 50% activation is
+                # found?
+        kc_selectors = nn.ModuleList()
+        flatsize = input_size[0]*input_size[1]*input_size[2]
+        for _ in kc_repititions:
+            # Generate a random mapping by selecting a single input for each output of a matrix
+            choices = shuffle(list(range(flatsize)))
+            selector = torch.zeros([flatsize, flatsize])
         # Return the convolution layers and the output size
         return nn.Sequential(*block), input_size
 
     def forward(self, x):
         # First, run through the convolutions
         x = self.features(x)
-        # Now, flatten and assign to positions in the KC bank
+        # Now, flatten and assign to positions in the KC bank with some masked selects
         x = x.flatten()
-        # Now, assign the inputs to their outputs with a masked select
+        # Now, assign the inputs to their outputs with a multiplication
         x = torch.zeros(x.size())
         for mask in self.kc_masks:
             x = x + torch.masked_select(x, mask)
