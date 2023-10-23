b15e5802a97838dbb9df4c4b99272f0e083a04e8
Richard P. Martin <rmartin@scarletmail.rutgers.edu>
2023-10-22 23:07:42 -0400

    added features to make dataset 6

--- make_validation_training.py
+++ make_validation_training.py
@@ -39,7 +39,7 @@ dataPrepProgram = os.path.join(program_dir, "VidActRecDataprep.py")
 trainProgram =  os.path.join(program_dir, 'VidActRecTrain.py')
 
 # command to run the data prep program
-dataPrepCommand = 'python3 $DATAPREPPROGRAM --width $WIDTH --height $HEIGHT --crop_x_offset $X_OFFSET --crop_y_offset $Y_OFFSET --resize-strategy crop --samples 500 --crop_noise 20 --out_channels 1 --frames_per_sample 1 $DATASETNAME.csv $DATASETNAME.tar'
+dataPrepCommand = 'python3 $DATAPREPPROGRAM --width $WIDTH --height $HEIGHT --crop_x_offset $X_OFFSET --crop_y_offset $Y_OFFSET --resize-strategy crop --samples 500 --crop_noise 10 --out_channels 1 --frames_per_sample 1 $DATASETNAME.csv $DATASETNAME.tar'
 # command to run the evaluation and training program 
 trainCommand    = 'srun -G 1 python3 $TRAINPROGRAM --not_deterministic --epochs 10 --modeltype $MODEL --evaluate' # <eval-set> <a-set> <b-set> ... 
 
