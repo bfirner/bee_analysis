9aef7214c6383b902f6466ad9ecf217100123f16
Ben Firner <ben.firner@gmail.com>
2023-11-30 18:18:48 -0500

    Playing around with partially presolved layers, no good results.

--- models/modules.py
+++ models/modules.py
@@ -56,12 +56,15 @@ class Normalizer(nn.Module):
 class PresolvedConv2d(nn.Module):
     """A 2d convolution layer with some parameters presolved."""
 
-    def __init__(self, in_channels, out_channels, presolved_weights, presolved_bias, stride=1,
+    def __init__(self, in_channels, out_channels, presolved_weights, presolved_bias, kernel_size, stride=1,
             padding=0, dilation=1, groups=1):
         super(PresolvedConv2d, self).__init__()
 
         # The weights and bias must be the same size in their first dimension
         assert presolved_weights.size(0) == presolved_bias.size(0)
+
+        # Weights should have size: [out_channels, in_channels, height, width]
+        solved_channels = presolved_weights.size(0)
 
         self.solved_weight = nn.parameter.Parameter(data=presolved_weights, requires_grad=False)
         self.solved_bias = nn.parameter.Parameter(data=presolved_bias, requires_grad=False)
@@ -73,11 +76,11 @@ class PresolvedConv2d(nn.Module):
             'groups': groups
         }
 
-        self.learned_conv = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, **self.conv_arguments)
+        self.learned_conv = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels-solved_channels, kernel_size=kernel_size, **self.conv_arguments)
 
     def forward(self, x):
         """Forward through the convs."""
-        presolved = torch.nn.functional.conv2d(x, self.solved_weight, self.solved_bias, **self.conv_arguments)
+        presolved = torch.nn.functional.conv2d(input=x, weight=self.solved_weight.data, bias=self.solved_bias.data, **self.conv_arguments)
         learned = self.learned_conv(x)
 
         return torch.cat((presolved, learned), dim=1)
@@ -87,19 +90,39 @@ class PresolvedLinear(nn.Module):
     """A linear layer with some parameters presolved."""
 
     def __init__(self, in_features, out_features, presolved_weights, presolved_bias):
-        super(PresolvedConv2d, self).__init__()
+        super(PresolvedLinear, self).__init__()
 
         # The weights and bias must be the same size in their first dimension
         assert presolved_weights.size(0) == presolved_bias.size(0)
 
+        # Weights should have size: [out_features, in_features]
+        solved_channels = presolved_weights.size(0)
+
         self.solved_weight = nn.parameter.Parameter(data=presolved_weights, requires_grad=False)
         self.solved_bias = nn.parameter.Parameter(data=presolved_bias, requires_grad=False)
 
-        self.learned_linear = torch.nn.Linear(in_features=in_features, out_features=out_features)
+        self.learned_linear = torch.nn.Linear(in_features=in_features, out_features=out_features-solved_channels)
 
     def forward(self, x):
         """Forward through the linear layers."""
-        presolved = torch.nn.functional.lienar(x, weight=self.solved_weight, bias=self.solved_bias)
+        presolved = torch.nn.functional.linear(x, weight=self.solved_weight, bias=self.solved_bias)
         learned = self.learned_linear(x)
 
         return torch.cat((presolved, learned), dim=1)
+
+
+class MaxThresholding(nn.Module):
+    """A module that applies a threshold based upon the maximum value."""
+
+    def __init__(self, alpha=0.9):
+        """Anything less than 0.9 * max will be converted to 0. Outputs will be (input - alpha*max)"""
+        super(MaxThresholding, self).__init__()
+        self.alpha = 0.9
+        self.activation = torch.nn.ReLU()
+
+    def forward(self, x):
+        """Outputs will be 0 or (input - alpha*max)."""
+        # Assume that the first dimension is a batch dimension.
+        threshold = torch.max(x.flatten(1), dim=1)[0].view(x.size(0), *((x.dim()-1)*[1])).expand(x.size())
+        return self.activation(torch.sub(x, threshold, alpha=self.alpha))
+
