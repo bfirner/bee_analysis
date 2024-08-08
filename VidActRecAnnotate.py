Fixing normalization. Could have sworn that this was already fixed.

VidActRecAnnotate.py
@@ -400,7 +400,8 @@ class VideoAnnotator:
                 # Visualization masks are not supported with all model types yet.
                 with torch.no_grad():
                     if args.normalize:
-                        v, m = torch.var_mean(image_input)
+                        # Normalize per channel, so compute over height and width
+                        v, m = torch.var_mean(image_input, dim=(image_input.dim()-2, image_input.dim()-1), keepdim=True)
                         net_input = (image_input - m) / v
                     else:
                         net_input = image_input

VidActRecTrain.py
@@ -610,7 +610,8 @@ if args.evaluate is not None:
                     net_input = torch.cat(raw_input, dim=1)
                 # Normalize inputs: input = (input - mean)/stddev
                 if args.normalize:
-                    v, m = torch.var_mean(net_input)
+                    # Normalize per channel, so compute over height and width
+                    v, m = torch.var_mean(net_input, dim=(net_input.dim()-2, net_input.dim()-1), keepdim=True)
                     net_input = (net_input - m) / v
 
                 vector_input=None

utility/train_utility.py
@@ -233,7 +233,8 @@ def trainEpoch(net, optimizer, scaler, label_handler,
                 net_input = torch.cat(raw_input, dim=1)
             # Normalize inputs: input = (input - mean)/stddev
             if normalize_images:
-                    v, m = torch.var_mean(net_input)
+                    # Normalize per channel, so compute over height and width
+                    v, m = torch.var_mean(net_input, dim=(net_input.dim()-2, net_input.dim()-1), keepdim=True)
                     net_input = (net_input - m) / v
 
             if encode_position:
@@ -313,7 +314,8 @@ def evalEpoch(net, label_handler, eval_stats, eval_dataloader, vector_range, tra
                 net_input = torch.cat(raw_input, dim=1)
             # Normalize inputs: input = (input - mean)/stddev
             if normalize_images:
-                v, m = torch.var_mean(net_input)
+                # Normalize per channel, so compute over height and width
+                v, m = torch.var_mean(net_input, dim=(net_input.dim()-2, net_input.dim()-1), keepdim=True)
                 net_input = (net_input - m) / v
 
             with torch.cuda.amp.autocast():