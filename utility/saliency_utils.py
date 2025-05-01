3ffd995f93c73c08aa94e641f95e78ccc32611ce
Restyled.io <commits@restyled.io>
2025-05-01 18:37:01 +0000

    Restyled by isort

--- utility/saliency_utils.py
+++ utility/saliency_utils.py
@@ -1,10 +1,11 @@
 import os
-import torch
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 
 
 def get_layer_by_name(model, layer_name):
